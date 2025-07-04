9268400fea748ea30f8d1bceb68fc37eedbd442b
jeevankumard24a <jeevankumard24a@gmail.com>
2025-07-04 11:00:36 +0530

    First Commit to Nodejs

---
+++ nodep1/server.js
@@ -0,0 +1,34 @@
+import express, {json} from 'express'
+import cors from 'cors';
+// Enable CORS for all origins
+const app= express()
+const PORT=5005
+app.use(cors());
+
+app.use(express.json())
+
+app.get('/firstapi',(req,res)=>{
+    const s= {
+        name: 'Reacttt',
+        age: 30,
+        city: 'New York'
+    }
+    res.send(s)
+})
+
+app.get('/secondapi',(req,res)=>{
+    const s= {
+        name: 'Second',
+        age: 30,
+        city: 'New York'
+    }
+    res.send(s)
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on porttt ${PORT}`)
+})
+
+
+
